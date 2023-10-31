d5c99b01f767363d2dab56b0a27e789af023a7ee
Millowise01 <millowise01@yahoo.co>
2023-10-31 00:22:19 -0700

    first five

---
+++ 0x01-python-if_else_loops_functions/list.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - the singly linked list
+ * @n: the interger
+ * @next: the pointer to the next node
+ * Description: the singly linkeed list mode structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_s;
+
+void free_listint(listint_t *head);
+size_t print_listint(const listint_t *h);
+listint_t *insert_node(listint_t **head, int number);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+
+#endif
