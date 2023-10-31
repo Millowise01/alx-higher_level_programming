74bbff8bbbcb2ddbca3adb50653430d6419eab47
Millowise01 <millowise01@yahoo.co>
2023-10-31 15:58:04 -0700

    Edited

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - to check if there is a circcle in he list
+ * @list: the pointer to the list's first node
+ * Return: 1 if true and 0 if false
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *lead = list, *next_node = list;
+
+	while (next_node != NULL && lead != NULL && lead->next != NULL)
+	{
+		next_node = next_node->next;
+		lead = lead->next->next;
+
+		if (next_node == lead)
+			return (1);
+	}
+
+	return (0);
+}
