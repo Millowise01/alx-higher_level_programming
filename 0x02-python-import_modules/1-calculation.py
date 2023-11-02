233408450698ac3b82d12205515c184364541e03
Millowise01 <millowise01@yahoo.co>
2023-11-02 04:03:26 -0700

    edited 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-def main():
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-
-    add_ans = add(a, b)
-    sub_ans = sub(a, b)
-    mul_ans = mul(a, b)
-    div_ans = div(a, b)
-
-    print("{:d} + {:d} = {:d}".format(a, b, add_ans))
-    print("{:d} - {:d} = {:d}".format(a, b, sub_ans))
-    print("{:d} * {:d} = {:d}".format(a, b, mul_ans))
-    print("{:d} / {:d} = {:d}".format(a, b, div_ans))
-
-if __name__ == "__main__":
-    main()
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
