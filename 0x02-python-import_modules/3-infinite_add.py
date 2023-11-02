4327a29090a22a2f30f4f08b97bc6ba1c5581a6f
Millowise01 <millowise01@yahoo.co>
2023-11-02 05:18:26 -0700

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-
-def main():
-    argc = len(argv) - 1
-    sm = 0
-
-    if argc == 0:
-        print("0")
-        return
-
-    for i in range(1, argc + 1):
-        sm += int(argv[i])
-
-    print("{}".format(sm))
-
 if __name__ == "__main__":
-    main()
+    import sys, math
+    result = 0
+    for i in sys.argv:
+        result += int(i)
+        print("{}".format(result))
